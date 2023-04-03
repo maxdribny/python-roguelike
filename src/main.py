49748d3d068ccc5fb1fc1eb584cea540aaf16782
maxdribny <mdribnyi@gmail.com>
2023-04-03 20:20:55 +0400

    First Commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+def main():
+    """
+    This is the main function
+    """
+    print("Hello World")
+
+
+# Press the green button in the gutter to run the script.
+if __name__ == '__main__':
+    main()
