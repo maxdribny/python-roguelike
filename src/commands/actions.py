84aff3d596920d90fbb44d8735f3c130534a6a70
Max <mdribnyi@gmail.com>
2023-04-23 20:05:08 +0400

    Refactored actions.py

--- src/commands/actions.py
+++ src/commands/actions.py
@@ -4,7 +4,7 @@ Copyright: 2023.
 
 from __future__ import annotations
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Tuple
 
 if TYPE_CHECKING:
     from entities.entity import Entity
@@ -16,7 +16,18 @@ class Action:
     An action is a command that can be performed by an entity.
     """
 
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def __init__(self, entity: Entity) -> None:
+        super().__init__()
+        self.entity = entity
+
+    @property
+    def engine(self) -> Engine:
+        """
+        Return the engine this action is being performed in.
+        """
+        return self.entity.game_map.engine
+
+    def perform(self) -> None:
         """
         Perform this action with the objects needed to determine its scope.
         'self.engine' is the scope this action is being performed in.
@@ -34,28 +45,37 @@ class EscapeAction(Action):
     An action to exit the game.
     """
 
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def perform(self) -> None:
         raise SystemExit()
 
 
 class ActionWithDirection(Action):
-    def __init__(self, dx: int, dy: int):
-        super().__init__()
+    def __init__(self, entity: Entity, dx: int, dy: int):
+        super().__init__(entity)
 
         self.dx = dx
         self.dy = dy
 
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    @property
+    def dest_xy(self) -> Tuple[int, int]:
+        """
+        Return the destination x, y this action is moving towards.
+        :return:
+        """
+        return self.entity.x + self.dx, self.entity.y + self.dy
+
+    @property
+    def blocking_entity(self) -> Entity:
+        """Return the blocking entity at this actions' destination."""
+        return self.engine.game_map.get_blocking_entity_at_location(*self.dest_xy)
+
+    def perform(self) -> None:
         raise NotImplementedError()
 
 
 class MeleeAction(ActionWithDirection):
-    def perform(self, engine: Engine, entity: Entity) -> None:
-        dest_x = entity.x + self.dx
-        dest_y = entity.y + self.dy
-
-        target = engine.game_map.get_blocking_entity_at_location(dest_x, dest_y)
-
+    def perform(self) -> None:
+        target = self.blocking_entity
         if not target:
             return  # No entity to attack
 
@@ -67,28 +87,24 @@ class MovementAction(ActionWithDirection):
     An action to move an entity.
     """
 
-    def perform(self, engine: Engine, entity: Entity) -> None:
+    def perform(self) -> None:
 
-        dest_x = entity.x + self.dx
-        dest_y = entity.y + self.dy
+        dest_x, dest_y = self.dest_xy
 
-        if not engine.game_map.in_bounds(dest_x, dest_y):
+        if not self.engine.game_map.in_bounds(dest_x, dest_y):
             return  # Destination out of bounds.
-        if not engine.game_map.tiles["walkable"][dest_x, dest_y]:
+        if not self.engine.game_map.tiles["walkable"][dest_x, dest_y]:
             return  # Destination is blocked by a tile.
-        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
+        if self.engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
             return  # Destination is blocked by an entity.
 
         # Otherwise move
-        entity.move(self.dx, self.dy)
+        self.entity.move(self.dx, self.dy)
 
 
 class BumpAction(ActionWithDirection):
-    def perform(self, engine: Engine, entity: Entity) -> None:
-        dest_x = entity.x + self.dx
-        dest_y = entity.y + self.dy
-
-        if engine.game_map.get_blocking_entity_at_location(dest_x, dest_y):
-            return MeleeAction(self.dx, self.dy).perform(engine, entity)
+    def perform(self) -> None:
+        if self.blocking_entity:
+            return MeleeAction(self.entity, self.dx, self.dy).perform()
         else:
-            return MovementAction(self.dx, self.dy).perform(engine, entity)
+            return MovementAction(self.entity, self.dx, self.dy).perform()
