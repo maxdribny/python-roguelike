693d34d5b25e7bc4ed5df5024fec304e0d43c732
Max <mdribnyi@gmail.com>
2023-04-23 22:03:06 +0400

    Refactored engine.py to remove the event handling components as these are managed by the  event handler class now

--- src/engine/engine.py
+++ src/engine/engine.py
@@ -1,14 +1,18 @@
 """Author: Maxim Dribny 2023"""
 
-from typing import Iterable, Any
+from __future__ import annotations
+
+from typing import TYPE_CHECKING
 
 from tcod.console import Console
 from tcod.context import Context
 from tcod.map import compute_fov
 
-from engine.game_map import GameMap
 from engine.input_handler import EventHandler
-from entities.entity import Entity
+
+if TYPE_CHECKING:
+    from entities.entity import Entity
+    from engine.game_map import GameMap
 
 
 class Engine:
@@ -16,15 +20,15 @@ class Engine:
     The main game engine class that holds and operates on the game state.
     """
 
-    def __init__(self, event_handler: EventHandler, game_map: GameMap, player: Entity):
+    game_map: GameMap
+
+    def __init__(self, player: Entity):
         """
         Initializes a new Engine object.
         :param player: Entity The player entity.
         """
-        self.event_handler = event_handler
-        self.game_map = game_map
+        self.event_handler = EventHandler(self)
         self.player = player
-        self.update_fov()
 
     def handle_enemy_turns(self) -> None:
         """
@@ -32,21 +36,6 @@ class Engine:
         """
         for entity in self.game_map.entities - {self.player}:
             print(f"The {entity.name} wonders when it will get to take a real turn.")
-
-    def handle_events(self, events: Iterable[Any]) -> None:
-        """
-        Handle events from the input handler.
-        :param events:
-        """
-        for event in events:
-            action = self.event_handler.dispatch(event)
-
-            if action is None:
-                continue
-
-            action.perform(self, self.player)
-            self.handle_enemy_turns()
-            self.update_fov()  # Update the FOV before the players next action.
 
     def update_fov(self) -> None:
         """Recompute the visible area based on the players point of view."""
